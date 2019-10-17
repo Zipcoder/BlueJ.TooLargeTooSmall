a9e434d017a1f192a9ec790aa601abd5ce16cc89
Eddie Morales <eddie@zipcoders-MacBook-Pro.local>
2019-10-16 20:31:56 -0400

    Solved test case

--- TooLargeTooSmallTest.java
+++ TooLargeTooSmallTest.java
@@ -5,52 +5,26 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * The test class TooLargeTooSmallTest.
- *
- * @author  (your name)
- * @version (a version number or a date)
- */
 public class TooLargeTooSmallTest
 {
-    /**
-     * Default constructor for test class TooLargeTooSmallTest
-     */
-    public TooLargeTooSmallTest()
-    {
-    }
-
-    /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
-     */
-    @Before
-    public void setUp()
-    {
-    }
-
-    /**
-     * Tears down the test fixture.
-     *
-     * Called after every test case method.
-     */
-    @After
-    public void tearDown()
-    {
-    }
-
     @Test
     public void TestRun1()
     {
+        // given
         TooLargeTooSmall tooLarge1 = new TooLargeTooSmall(5);
+        
+        // when
+        
+        //then
         assertEquals(Integer.valueOf(0), tooLarge1.guess(5));
     }
     
     @Test
     public void TestRun2()
     {
+        //given
         TooLargeTooSmall tooLarge1 = new TooLargeTooSmall(6);
+        //when
         assertEquals(Integer.valueOf(-1), tooLarge1.guess(4));
     }
     
