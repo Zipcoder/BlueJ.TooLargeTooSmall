58bdac2e7fbe6d20c1f33a492da54eaa68a32b33
Jbanksalpha <78812718+Jbanksalpha@users.noreply.github.com>
2021-02-25 18:19:34 -0500

    ran tests for too large too small

--- TooLargeTooSmall.java
+++ TooLargeTooSmall.java
@@ -7,6 +7,7 @@
  */
 public class TooLargeTooSmall {
     private Integer chosenNum;
+    
 
     /**
      * Constructor for objects of class TooLargeTooSmall
@@ -14,11 +15,22 @@ public class TooLargeTooSmall {
     public TooLargeTooSmall(Integer seed) {
         // initialise instance variables
         chosenNum = seed;
+        
+        
     }
     
     public Integer guess(Integer g) {
+        if (g < chosenNum) {
+            return -1;
+        } else if (g > chosenNum) {
+            return 1;
+        } else {
+            return 0;
+        }
+            
         
-        return chosenNum;
+            
+        
 
     }
 }
