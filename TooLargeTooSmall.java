38a7b47fa73f5aa0b59b968f46690a552390e19c
Dee <dee@zipcodes-MacBook-Pro-2.local>
2021-06-26 14:44:36 -0400

    TooLargeTooSmallNot

--- TooLargeTooSmall.java
+++ TooLargeTooSmall.java
@@ -13,12 +13,22 @@ public class TooLargeTooSmall {
      */
     public TooLargeTooSmall(Integer seed) {
         // initialise instance variables
-        chosenNum = seed;
+        chosenNum = seed; 
     }
     
     public Integer guess(Integer g) {
+        //if guess is greater than chosenNum, then chosenNum = 1
+        //if less than, then -1
+        //if equal then 0
         
-        return chosenNum;
+        int output; 
+        if (g > chosenNum){
+            output = 1;
+        } else if (g < chosenNum) {
+            output = -1;
+        } else {
+            output = 0; }
+        return output;
 
     }
 }
